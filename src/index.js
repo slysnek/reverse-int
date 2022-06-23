5bb751c8d7f27920337b28051012151fccaf7243
Kirill <bloodwingest@gmail.com>
2022-06-23 21:51:12 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    let Stringnumber = String(number); 
+    let splittedNumber = Stringnumber.split("");
+    let modifiedNumber = splittedNumber.reverse();
+    let rawNumber = modifiedNumber.join("");
+    let finalNumber = rawNumber.replace('-', '');
+    return finalNumber;
 }
